c3367cd03888ae5b1578b748b279b9a5e83bdfc2
JeongsooP <jspark702@gmail.com>
2022-03-06 17:32:05 -0500

    Update design_JP.py

--- experiment/design_JP.py
+++ experiment/design_JP.py
@@ -1,15 +1,17 @@
 # Model design for Jeongsoo Park
 # Github desktop test
 import torch
+import numpy as np
 
 class MLP_drop(torch.nn.Module):
     def __init__(self, input_size, hidden_sizes, output_size):
         super(MLP_drop, self).__init__()
-        self.input_size = input_size
+        self.input_size = np.prod(input_size)
         self.hidden_sizes  = hidden_sizes
         self.output_size = output_size
 
-        sizes = [input_size] + hidden_sizes + [output_size]
+        sizes = [self.input_size] + hidden_sizes + [output_size]
+        
         self.fcs = torch.nn.ModuleList([torch.nn.Linear(sizes[i], sizes[i+1]) for i in range(len(sizes)-1)])
         self.relu = torch.nn.ReLU()
         self.drop = torch.nn.Dropout(p=0.5)
@@ -17,6 +19,7 @@ class MLP_drop(torch.nn.Module):
     def forward(self, x):
         num = len(self.fcs)
         output = x
+        output = torch.flatten(x, start_dim=1)
         for i in range(num-1):
             output = self.fcs[i](output)
             output = self.relu(output)
